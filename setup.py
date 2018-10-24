caf1f90730471964d318b4d3f72960ff3831e8a8
Klaas Bosteels <klaas.bosteels@gmail.com>
2018-10-24 19:06:00 +0200

    Add description and url to setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ setup(
     name='jinx',
     version='0.1',
     license='Apache Software License (ASF)',
+    description='Fast and simple indexing for JSON Lines files',
+    url='https://github.com/trackuity/jinx',
     packages=find_packages(),
     install_requires=[
         'bsddb3==6.2.6',
